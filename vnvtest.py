54b92045de031140fc95490663488d75c4e7be25
Kanchana Bandara <60261547+Kanchana-Bandara@users.noreply.github.com>
2026-05-22 10:31:20 +0200

    C2KB: added model analyses and output drafts in 1D

---
+++ vnvtest.py
@@ -0,0 +1,22 @@
+print("venv test")
+
+#importing modules
+#/////////////////
+#these are necessary for performing computations (make sure that they are installed or set-up in the virtual environment before proceeding)
+import numpy as np
+import pathlib
+import netCDF4 as nc
+import math
+import time
+from datetime import datetime
+import os
+import hashlib
+import uuid
+import pascal42_mod_verticalmigration as vm
+import pascal42_mod_growthdevelopmentmetabolism as gdm
+import pascal42_mod_survival as sv
+import termcolor
+from datetime import datetime
+from time import gmtime, strftime
+import h5py
+import sys
